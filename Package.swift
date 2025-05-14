71d8452f359ff0210c055694f0820fea346ae3a8
Prince Canuma <prince.gdt@gmail.com>
2025-05-15 01:33:30 +0200

    Remove model not found and fix swift GH actions (#146)
    * remove model not found
    
    * bump
    
    * move swift files to folder
    
    * fix swift tests
    
    * cd into swift folder
    
    * fix swift folder
    
    * fix package
    
    * use xcodebuild
    
    * use xcode
    
    * fix package
    
    * update tests
    
    * fix GH actions
    
    * downgrade to macos 14
    
    * remove code signing
    
    * update tests
    
    * fix path
    
    * add clean build step
    
    * rename swift-tts-tests to tests
    
    * update test names

--- Package.swift
+++ Package.swift
@@ -2,12 +2,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "MLXSwiftTTS",
+    name: "Swift-TTS",
     platforms: [.macOS(.v14), .iOS(.v16)],
     products: [
         .library(
-            name: "MLXSwiftTTS",
-            targets: ["MLXSwiftTTS","ESpeakNG"]),
+            name: "Swift-TTS",
+            targets: ["Swift-TTS","ESpeakNG"]),
     ],
     dependencies: [
          .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.25.2")
@@ -15,19 +15,21 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ESpeakNG",
-            path: "mlx_audio_swift/tts/Swift-TTS/Kokoro/Frameworks/ESpeakNG.xcframework"
+            path: "Swift-TTS/Kokoro/Frameworks/ESpeakNG.xcframework"
         ),
         .target(
-            name: "MLXSwiftTTS",
+            name: "Swift-TTS",
             dependencies: [.product(name: "MLX", package: "mlx-swift"),
             .product(name: "MLXFFT", package: "mlx-swift"),
             .product(name: "MLXNN", package: "mlx-swift"),
              "ESpeakNG"
            ],
-            path: "mlx_audio_swift/tts/Swift-TTS",
+            path: "Swift-TTS",
             exclude: ["Preview Content","Assets.xcassets","Swift_TTSApp.swift","Swift_TTS.entitlements"]),
         .testTarget(
-            name: "MLXSwiftTTSTests",
-            dependencies: ["MLXSwiftTTS"]),
+            name: "Swift-TTS-Tests",
+            dependencies: ["Swift-TTS"],
+            path: "Tests"
+        ),
     ]
 )
